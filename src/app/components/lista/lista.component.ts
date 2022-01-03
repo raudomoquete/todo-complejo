3fb8b783e3fdea5e4871b6a19121ddc1b2621d44
Raudo Moquete <raudo.moquete@rhodium.com.do>
2022-01-03 15:13:10 -0500

    todo-complejo terminado

--- src/app/components/lista/lista.component.ts
+++ src/app/components/lista/lista.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Tarea } from 'src/models/tarea.models';
+
 
 @Component({
   selector: 'app-lista',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaComponent implements OnInit {
 
+  @Input() 
+  tareas: Tarea[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClick(pTarea: Tarea) {
+    pTarea.completa = !pTarea.completa;
+  }
+
+  onClickBorrar(indice: any) {
+    this.tareas.splice(indice, 1);
   }
 
 }
